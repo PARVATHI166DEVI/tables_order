d356299f0a298874c887edd82e62ab85503fdd5f
PARVATHI166DEVI <66837819+PARVATHI166DEVI@users.noreply.github.com>
2021-12-08 22:51:40 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,33 +1,20 @@
-var b = 1;
-var s = "";
-function cal(){
+let color = ["red","green","blue","grey","black","SlateBlue","Orange","DeepPink","violet","yellow"];
+let b = 1;
+function demo(){
+    let s = "";
     for(let i=1;i<=10;i++){
-        s = (i*b)+" ";
-        if(b==1)
-            document.write(s.fontcolor("green"));
-        if(b==2)
-            document.write(s.fontcolor("red"));
-        if(b==3)
-            document.write(s.fontcolor("blue"));
-        if(b==4)
-            document.write(s.fontcolor("Orange"));
-        if(b==5)
-            document.write(s.fontcolor("yellow"));
-        if(b==6)
-            document.write(s.fontcolor("SlateBlue"));
-        if(b==7)
-            document.write(s.fontcolor("black"));
-        if(b==8)
-            document.write(s.fontcolor("Chartreuse"));        
-        if(b==9)
-            document.write(s.fontcolor("brown"));
-        if(b==10)
-            document.write(s.fontcolor("DeepPink"));
+        s += (i*b) + " ";
     }
-    if (b >= 10)
-        clearInterval(timer);
+    let ele = document.createElement('div');
+    ele.innerText = s;
+    ele.style.fontSize = b*3+20+"px";
+    ele.style.textAlign = "center";
+    ele.style.color = color[b-1];
+    document.body.appendChild(ele);
+    if(b>=10)
+        clearInterval(set_interval);
     b++;
     document.write("<br>");
 };
-var timer = setInterval(cal, 5000);
 
+var set_interval = setInterval(demo,5000);
